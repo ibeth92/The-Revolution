37016e05011a164d124522d864eb003c88ad63f4
Stephanie Plaza <66174042+sjplaza@users.noreply.github.com>
2020-12-15 19:16:38 -0600

    updating files

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,7 @@
 function buildPlot() {
   /* data route */
   const url = "/api/pals";
-  d3.json(url).then(function(response) {
+  d3.json(url).then(function (response) {
 
     console.log(response);
 
@@ -12,7 +12,7 @@ function buildPlot() {
       title: "Pet Pals",
       showlegend: false,
       height: 600,
-            // width: 980,
+      // width: 980,
       geo: {
         scope: "usa",
         projection: {
@@ -32,3 +32,27 @@ function buildPlot() {
 }
 
 buildPlot();
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById('myImg');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function () {
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  modalImg.alt = this.alt;
+  captionText.innerHTML = this.alt;
+}
+
+// When the user clicks on <span> (x), close the modal
+modal.onclick = function () {
+  img01.className += " out";
+  setTimeout(function () {
+    modal.style.display = "none";
+    img01.className = "modal-content";
+  }, 400);
+
+}    
