de2abb8475b99bbe83b18392c7251035377eb625
Ibet Hernandez <ibethernandez92@gmail.com>
2020-12-17 19:12:32 -0600

    app

--- app.py
+++ app.py
@@ -1,80 +1,29 @@
-# import necessary libraries
-from models import create_classes
+# Import dependencies
 import os
-from flask import (
-    Flask,
-    render_template,
-    jsonify,
-    request,
-    redirect)
+import json
+import collections
+import flask 
+from flask import Flask, jsonify, render_template, request
+from flask_cors import cross_origin 
 
-#################################################
-# Flask Setup
-#################################################
+# Setup Flask
+# Create an app, pass to __name__
 app = Flask(__name__)
 
-#################################################
-# Database Setup
-#################################################
-
-from flask_sqlalchemy import SQLAlchemy
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '') or "sqlite:///db.sqlite"
-
-# Remove tracking modifications
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
-
-Pet = create_classes(db)
-
-# create route that renders index.html template
-@app.route("/")
-def home():
-    return render_template("index.html")
+# app.config['UPLOAD_FOLDER'] = 
 
 
-# Query the database and send the jsonified results
-@app.route("/send", methods=["GET", "POST"])
-def send():
-    if request.method == "POST":
-        name = request.form["petName"]
-        lat = request.form["petLat"]
-        lon = request.form["petLon"]
+@app.route("/", methods=['GET'])
+@cross_origin()
+def homepage():
+    return render_template('index.html')    
 
-        pet = Pet(name=name, lat=lat, lon=lon)
-        db.session.add(pet)
-        db.session.commit()
-        return redirect("/", code=302)
-
-    return render_template("form.html")
-
-
-@app.route("/api/pals")
-def pals():
-    results = db.session.query(Pet.name, Pet.lat, Pet.lon).all()
-
-    hover_text = [result[0] for result in results]
-    lat = [result[1] for result in results]
-    lon = [result[2] for result in results]
-
-    pet_data = [{
-        "type": "scattergeo",
-        "locationmode": "USA-states",
-        "lat": lat,
-        "lon": lon,
-        "text": hover_text,
-        "hoverinfo": "text",
-        "marker": {
-            "size": 50,
-            "line": {
-                "color": "rgb(8,8,8)",
-                "width": 1
-            },
-        }
-    }]
-
-    return jsonify(pet_data)
-
+# Set up Upload File 
+@app.route('/uploader', methods = ['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['file']
+        return 'file uploaded successfully'
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
